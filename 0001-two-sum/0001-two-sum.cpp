a7a42ff3a0867f31e78539899bbd63c85a63fe5f
Avik Sarkar <aviksarkar0204@gmail.com>
2026-08-25 10:41:42 +0530

    Time: 0 ms (100%), Space: 14.8 MB (58.18%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // store (value, original_index) pairs
+        vector<pair<int,int>> indexed;
+        for (int i = 0; i < nums.size(); i++) {
+            indexed.push_back({nums[i], i});
+        }
+
+        // sort by value (pair sorts by first element by default)
+        sort(indexed.begin(), indexed.end());
+
+        int left = 0;
+        int right = indexed.size() - 1;
+
+        while (left < right) {
+            int sum = indexed[left].first + indexed[right].first;
+            if (sum == target) {
+                return {indexed[left].second, indexed[right].second};
+            }
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return {};
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
