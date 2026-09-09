e4adbae5096993461dfa0f6ca97e84128253a2d8
Avik Sarkar <aviksarkar0204@gmail.com>
2026-09-09 11:19:14 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.67%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int slow_ptr = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if (nums[i] != 0) {
+                nums[slow_ptr] = nums[i];
+                slow_ptr++;
+            }
+        }
+        while(slow_ptr < nums.size()){
+            nums[slow_ptr] = 0;
+            slow_ptr++;
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
