3f599cd82ee7f3cf794e0b6606c9812942b218c9
Avik Sarkar <aviksarkar0204@gmail.com>
2026-09-14 11:17:10 +0530

    Time: 26 ms (88.37%), Space: 16 MB (31.32%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
@@ -17,6 +18,42 @@ public:
                 }
                 maxCount = max(maxCount, count);
             }
+        }
+        return maxCount;
+    }
+};
+*/
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        vector<pair<int, int>> oneA;
+        vector<pair<int, int>> oneB;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (img1[i][j] == 1) {
+                    oneA.push_back({i, j});
+                }
+                if (img2[i][j] == 1) {
+                    oneB.push_back({i, j});
+                }
+            }
+        }
+        int B =
+            2 * n; // safe base, since dy_shifted ranges 0 to 2n-2, always < 2n
+        unordered_map<int, int> shiftCount;
+        for (auto& a : oneA) {
+            for (auto& b : oneB) {
+                int dx = b.first - a.first;
+                int dy = b.second - a.second;
+                int key = (dx + n - 1) * B +
+                          (dy + n - 1); // shift to non-negative, then encode
+                shiftCount[key]++;
+            }
+        }
+        int maxCount = 0;
+        for (auto& entry : shiftCount) {
+            maxCount = max(maxCount, entry.second);
         }
         return maxCount;
     }
