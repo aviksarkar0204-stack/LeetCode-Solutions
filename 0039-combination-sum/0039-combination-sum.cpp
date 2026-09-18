3f1ff664776af2b9116f8606f5ed43c70ebbe159
Avik Sarkar <aviksarkar0204@gmail.com>
2026-09-18 23:05:50 +0530

    Time: 7 ms (24.2%), Space: 18.9 MB (17.58%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> cur;
+
+        function<void(int,int)> dfs = [&](int i, int total) {
+            if (total == target) {
+                res.push_back(cur);
+                return;
+            }
+            if (i >= candidates.size() || total > target) {
+                return;
+            }
+
+            // include candidates[i]
+            cur.push_back(candidates[i]);
+            dfs(i, total + candidates[i]); // stay at i (can reuse same number)
+            cur.pop_back();
+
+            // skip candidates[i]
+            dfs(i + 1, total);
+        };
+
+        dfs(0, 0);
+        return res;
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
