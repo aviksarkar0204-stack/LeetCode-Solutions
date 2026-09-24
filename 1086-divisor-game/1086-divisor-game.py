b4023637edae51e322b92587f6a190b30a3b67ae
Avik Sarkar <aviksarkar0204@gmail.com>
2026-09-24 10:01:54 +0530

    Time: 0 ms (100%), Space: 19.2 MB (84.13%) - LeetHub

---
+++ 1086-divisor-game/1086-divisor-game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        return n % 2 == 0
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
